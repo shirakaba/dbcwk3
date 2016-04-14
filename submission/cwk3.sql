b9e1389ffe1be26312a2088654d90836e6a5e90b
pn15644 <pn15644@my.bristol.ac.uk>
2016-04-14 18:13:17 +0100

    sql file
    create table

---
+++ submission/cwk3.sql
@@ -0,0 +1,50 @@
+DROP TABLE IF EXISTS Person;
+DROP TABLE IF EXISTS Topic;
+DROP TABLE IF EXISTS Forum;
+DROP TABLE IF EXISTS Post;
+DROP TABLE IF EXISTS LikedPost;
+DROP TABLE IF EXISTS LikedTopic;
+DROP TABLE IF EXISTS FavouritedTopic;
+
+
+CREATE TABLE Person ( -- This schema is set by David
+  id INTEGER PRIMARY KEY,
+  username VARCHAR(10) NOT NULL UNIQUE,
+  name VARCHAR(100) NOT NULL,
+  studentId VARCHAR(10) NULL
+);
+
+CREATE TABLE Topic (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  postId INTEGER REFERENCES Post(id)
+);
+
+CREATE TABLE Forum (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL UNIQUE,
+  topicId INTEGER REFERENCES Topic(id)
+);
+
+CREATE TABLE Post (
+  id INTEGER PRIMARY KEY,
+  date DATE NOT NULL,
+  text TEXT NOT NULL,
+  personId INTEGER REFERENCES Person(id),
+  topicId INTEGER REFERENCES Topic(id)
+);
+
+CREATE TABLE LikedPost (
+  PostId INTEGER REFERENCES Post(id),
+  PersonId INTEGER REFERENCES Person(id)
+);
+
+CREATE TABLE LikedTopic (
+  TopicId INTEGER REFERENCES Topic(id),
+  PersonId INTEGER REFERENCES Person(id)
+);
+
+CREATE TABLE FavouritedTopic (
+  TopicId INTEGER REFERENCES Topic(id),
+  PersonId INTEGER REFERENCES Person(id)
+);

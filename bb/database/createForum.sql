2955ccd2770ed626168e4a9399710f5a0507e1a6
Alexander Lorimer <al15273@it025984.users.bris.ac.uk>
2016-04-26 13:55:46 +0100

    populated database, excepted for join tables

---
+++ bb/database/createForum.sql
@@ -0,0 +1,3 @@
+INSERT INTO Forum (title) VALUES ('Chocolate Lovers');
+INSERT INTO Forum (title) VALUES ('Chocolate Haters');
+INSERT INTO Forum (title) VALUES ('Caesar\'s Forum');
---
+++ bb/database/createTopicANDCreatePost.sql
@@ -0,0 +1,22 @@
+INSERT INTO Topic (title, ForumId) VALUES ('Wonka Bar', 1);
+INSERT INTO Topic (title, ForumId) VALUES ('Snickers', 1);
+
+INSERT INTO Topic (title, ForumId) VALUES ('Calories', 2);
+INSERT INTO Topic (title, ForumId) VALUES ('Diabetes', 2);
+
+INSERT INTO Topic (title, ForumId) VALUES ('Roman Empire', 3);
+INSERT INTO Topic (title, ForumId) VALUES ('Gladiator', 3);
+
+INSERT INTO Post (date, text, PersonId, TopicId) VALUES (1461673720, "Wonka Bars are like, so last season... am I right?... am I right?!", 1, 1);
+
+INSERT INTO Post (date, text, PersonId, TopicId) VALUES (1461671720, "Do Snickers bars have nuts them? I'm allergic and I\'ve gone all puffy after eating one...", 3, 2);
+
+INSERT INTO Post (date, text, PersonId, TopicId) VALUES (1461670720, "How many calories are there in a kilogram of milk chocolate... generally?", 2, 3);
+
+INSERT INTO Post (date, text, PersonId, TopicId) VALUES (1461610720, "Is sugar bad?...", 2, 4);
+
+INSERT INTO Post (date, text, PersonId, TopicId) VALUES (1461510720, "Within what time period was the Roman Empire kicking ass?", 1, 5);
+
+INSERT INTO Post (date, text, PersonId, TopicId) VALUES (1461510720, "Is the Gladiator film with Russel Crow based on real events?", 3, 6);
+
+
